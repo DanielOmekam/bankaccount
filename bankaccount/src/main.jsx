8bc2d9124770baa873a94ad2411e1770666a8d51
Daniel Omekam <omekam.daniel@gmail.com>
2024-10-13 16:32:56 -0400

    bankact

--- bankaccount/src/main.jsx
+++ bankaccount/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import accountReducer from './redux/accountSlice';
+import accountReducer from './accountSlice';
 import App from './App';
 
 const store = configureStore({
